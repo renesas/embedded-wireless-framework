cb394fda87cbaa84a2f978e20e5dead113db2d68
Adam Benson <78607734+renesas-adam-benson@users.noreply.github.com>
2024-09-04 09:40:23 -0400

    LLVM Compile Fix (#2)
    * include ctype

--- src/ewf_adapter_renesas_common_internet.c
+++ src/ewf_adapter_renesas_common_internet.c
@@ -7,6 +7,8 @@
  * brief The Embedded Wireless Framework Renesas common Internet(TCP+UDP) implementation
  ****************************************************************************/
 
+#include <ctype.h>
+
 #include "ewf_adapter_renesas_common.h"
 #include "ewf_platform.h"
 #include "ewf_lib.h"
